ed013945b24032881ca7bb2a1dac901257ac9ecc
Dmitriy Samsonov <LeonSpace777@yandex.ru>
2023-01-28 13:17:33 +0300

    Task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int number)
+{
+    int sum = 0;
+
+    while (number > 0)
+{
+    int num = number % 10;
+    sum = sum + num;
+    number = number / 10;
+}
+    return sum;
+}
+
+System.Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write($"Сумма цифр числа {number} равна ");
+
+System.Console.WriteLine(Sum(number));
